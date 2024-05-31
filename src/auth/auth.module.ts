a213ac4d0ea32581368e50b8925dd3fd6e5bb84f
BOThiago <thiago.barreto.oliveira22@gmail.com>
2024-05-31 07:44:00 -0300

    hotfix: circular dependency & export providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
-import { UserService } from '../user/user.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
+    forwardRef(() => UserModule),
     PassportModule,
     JwtModule.register({
       secret: 'secretKey',
       signOptions: { expiresIn: '60m' },
     }),
   ],
-  providers: [AuthService, JwtStrategy, UserService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
